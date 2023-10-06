698485197766e8fbb35a83725d0a662585df5bcf
Jolie-rit <69928450+Jolie-rit@users.noreply.github.com>
2023-10-06 13:59:06 -0400

    Build
    fixed

--- Assets/Script/movement_test/MovementController_2D.cs
+++ Assets/Script/movement_test/MovementController_2D.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
-using UnityEditor.SceneManagement;
+//using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.Windows;
 
