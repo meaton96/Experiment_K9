12e143037bb5c72278b509ffd8b0c0b319ae9164
Jolie-rit <69928450+Jolie-rit@users.noreply.github.com>
2023-10-16 01:02:50 -0400

    Updating second wall behavior
    Added wall behavior that was on other branch before merge

--- Assets/Script/New/objects/WallBehaviour.cs
+++ Assets/Script/New/objects/WallBehaviour.cs
@@ -18,9 +18,62 @@ public class WallBehaviour : MonoBehaviour {
     public bool IsMovingWall = false;
 
     public float pushForce = 1f;
-    
+    //checks to see if its a movingwall
+    [SerializeField]
+    public bool movingWall = false;
+    //waiting for a bit before moving
+    private bool wait = false;
+    private float waittime=10f;
+    //how fast the wall is moving
+    public float MoveSpeed = 5.0f;
+    //the staring and ending points of the wall movement(has to stop at a wall before going back)
+    public Transform initialposition;
+    public Transform targetposition;
+    //tracks time
+    private float time;
     // This method controls conveyor walls, the walls that move the player in a
     // certain direction when they are merged into them.
+    void Start()
+    {
+
+        
+    }
+
+    
+    void Update()
+    {
+
+        if (movingWall == true)
+        {
+            //basically speed wall
+            float step = MoveSpeed * Time.deltaTime;
+            //this will move the wall at the constant speed towards the target
+            transform.position = Vector3.MoveTowards(transform.position, targetposition.position, step);
+            if (transform.position == targetposition.position)
+            {
+                //makes the wall wait a few seconds
+                wait = true;
+                //basically makes the wall go back and fourth between the two wall positions by swapping target and initial
+                Transform mid = initialposition;
+               
+                initialposition = targetposition;
+                targetposition = mid;
+                //allows the wait and resets time.
+                movingWall = false;
+                time = 0;
+            }
+        }
+        else if (wait == true)
+        {
+            //waitting and then allow wall to move again
+            time += Time.deltaTime;
+            if (time >= waittime)
+            {
+                movingWall = true;
+            }
+        }
+
+    }
     private void MovingWall()
     {
         WallForce.x = transform.right.x;
