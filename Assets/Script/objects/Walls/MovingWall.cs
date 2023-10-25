dfc5767ac31c6e32d7a034ca2c38625269e30d8a
meaton96 <lammara96@gmail.com>
2023-10-25 18:25:44 -0400

    moved moving wall to its own class

--- Assets/Script/objects/Walls/MovingWall.cs
+++ Assets/Script/objects/Walls/MovingWall.cs
@@ -1,26 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-public class WallBehaviour : MonoBehaviour {
-    //to check if the player can enter or leave 2d/3d space using this wall
-    public bool AllowsDimensionTransition = false;
-    //to check if the player can move into this object or not
-    public bool IsWalkThroughEnabled = true;
-    //to check if the player can pass through the wall while in 2D
-    public bool IsPassthrough = false;
-    //remove from all checks for player movement
-    public bool RemoveFromWalkChecks = false;
-
-    private Rigidbody player;
-    Vector2 WallForce = new Vector2();
+public class MovingWall : MonoBehaviour
+{
     [SerializeField]
-    public bool IsMovingWall = false;
-
-    public float pushForce = 1f;
-    //checks to see if its a movingwall
-    [SerializeField]
-    public bool movingWall = false;
+    public bool isMoving = false;
     //waiting for a bit before moving
     private bool wait = false;
     [SerializeField] private float waitTime = 10f;
@@ -29,22 +15,25 @@ public class WallBehaviour : MonoBehaviour {
     //the staring and ending points of the wall movement(has to stop at a wall before going back)
     public Transform initialposition;
     public Transform targetposition;
-
     //tracks time
     private float time;
     public float stopThreshold;
     private Vector3 stopLocalPosition;
-    // This method controls conveyor walls, the walls that move the player in a
-    // certain direction when they are merged into them.
-    void Start() {
+    Vector2 WallForce = new Vector2();
 
-
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void setWallX() {
+        WallForce.x = transform.right.x;
     }
 
-
-    void Update() {
-
-        if (movingWall == true) {
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMoving == true) {
             //basically speed wall
             float step = MoveSpeed * Time.deltaTime;
             //this will move the wall at the constant speed towards the target
@@ -57,7 +46,7 @@ public class WallBehaviour : MonoBehaviour {
                 //basically makes the wall go back and fourth between the two wall positions by swapping target and initial
                 (targetposition, initialposition) = (initialposition, targetposition);
                 //allows the wait and resets time.
-                movingWall = false;
+                isMoving = false;
                 time = 0;
             }
         }
@@ -65,21 +54,8 @@ public class WallBehaviour : MonoBehaviour {
             //waitting and then allow wall to move again
             time += Time.deltaTime;
             if (time >= waitTime) {
-                movingWall = true;
+                isMoving = true;
             }
-        }
-
-    }
-    private void OnCollisionEnter(Collision collision) {
-
-    }
-    private void MovingWall() {
-        WallForce.x = transform.right.x;
-    }
-
-    private void OnTriggerExit(Collider other) {
-        if (other.gameObject.layer == LayerInfo.PLAYER) {
-            player = null;
         }
     }
 }
