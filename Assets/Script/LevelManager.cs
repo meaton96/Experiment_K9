4f6b7c61790581b5fe136198d828b61fd9710f54
meaton96 <lammara96@gmail.com>
2023-11-02 23:15:34 -0400

    level manager change

--- Assets/Script/LevelManager.cs
+++ Assets/Script/LevelManager.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 
 public class LevelManager : MonoBehaviour {
     private int lvlNum = 1;
+    [SerializeField] private GameObject player3D;
     [SerializeField]
     PlayerBehaviour player;
     string[] sceneArray;
@@ -44,9 +45,11 @@ public class LevelManager : MonoBehaviour {
     }
 
     private void OnSceneLoaded(Scene scene, LoadSceneMode load) {
+        player3D.SetActive(false);
         //Debug.Log("teleporting in: " + SceneManager.GetActiveScene().name);
         if (SceneManager.GetActiveScene().name != "LevelNull")
             player.Move3DPlayerToLocation(GameObject.Find("Spawnpoint").transform.position);
+        player3D.SetActive(true);   
     }
 
     //private void InitializeCatelog() {
